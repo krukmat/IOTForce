065919a87f60ae929e15c3e5fb622187fd52d464
Matias Kruk <kruk.matias@gmail.com>
2021-11-09 16:32:50 +0100

    Changes in REST API: More generic

--- python/rest_app.py
+++ python/rest_app.py
@@ -1,28 +1,20 @@
 #!/usr/bin/python3
 import sys, os
-sys.path.append('/rest /var/www/html/GardenForce-Demo/python/')
+sys.path.append('/rest /var/www/html/IOTForce/python/')
 from flask import Flask, request
 import mqtt_proxy
 app = Flask(__name__)
 
 @app.route('/setup')
 def index():
-    value = request.args.get('moist', '')
-    parameter = 'moist'
-    if  value == '':
-        value = request.args.get('read_ms', '')
-        parameter = 'read_ms'
-    if value == '':
-        value = request.args.get('flow_ms', '')
-        parameter = 'flow_ms'
-        
-    plantid = request.args.get('plant_id')
-    message = plantid + ";" +  parameter +';'+value + ";MQTT"
-    if value and plantid:
-        client = mqtt_proxy.connect_mqtt()
-        mqtt_proxy.mqtt_publish(client, message)
-        return 'OK'
-    return 'KO'
+     deviceid = request.args.get('device_id')
+    for key in request.args.keys():
+        if deviceid and request.args[key]:
+            message = deviceid +  ";" +  key +';'+request.args[key] + ";MQTT"
+            client = mqtt_proxy.connect_mqtt()
+            mqtt_proxy.mqtt_publish(client, message)
+            return 'OK'
+    return 'KO' 
 
 @app.route('/execute')
 def execute_task():
--- python/rest_bridge.wsgi
+++ python/rest_bridge.wsgi
@@ -1,6 +1,6 @@
 #! /usr/bin/python3.6
 import sys
-sys.path.insert(0, '/rest /var/www/html/GardenForce-Demo/python/')
+sys.path.insert(0, '/rest //var/www/html/IOTForce/python/')
 import logging
 logging.basicConfig(stream=sys.stderr)
 from rest_app import app as application
