d7c1f8c2a87428211e65c55df10f2969c6d33bc6
Matias Kruk <kruk.matias@gmail.com>
2021-11-09 15:13:13 +0100

    Platform Event: DeviseMessage

--- python/mqtt_proxy.py
+++ python/mqtt_proxy.py
@@ -10,20 +10,7 @@ def on_connect(client, userdata, flags, rc):
     print("Connected with result code "+str(rc))
     client.subscribe("HPIbCG0C72lcw6g/input")
 
-def on_message(client, userdata, msg):
-    print(msg.topic+" "+str(msg.payload))
-    data = msg.payload
-    data = data.decode('utf8').replace("'", '"')
-    print(data)
-    # Evitar el loopback de MQTT
-    if  not(";MQTT" in data):
-        #TODO: Caso donde se envia PlantId;valor de humedad
-        if ";" in data:
-            parameters = data.split(";")
-            result = sf.sobjects.Plant__c.upsert("PlantId__c", parameters[0], {"current__c": parameters[1]}) #TODO: Platform events instead?
-        else:
-            # Caso que se recibe desde el ESP-32 para que se devuelva el valor desde SF
-            if "PLANT" in data:
+def getGardenForceData(data, client, userdata, msg):    
                 plant_id = data
                 try:
                     result = sf.sobjects.query("SELECT moist__c,refresh__c,flowMS__c FROM Plant__c WHERE PlantId__c = '"+plant_id+"'")
@@ -40,6 +27,26 @@ def on_message(client, userdata, msg):
                     
                 except:
                     print("Error")
+
+def on_message(client, userdata, msg):
+    print(msg.topic+" "+str(msg.payload))
+    data = msg.payload
+    data = data.decode('utf8').replace("'", '"')
+    print(data)
+    # Evitar el loopback de MQTT
+    if  not(";MQTT" in data):
+        #TODO: Caso donde se envia PlantId;valor de humedad
+        if ";" in data:
+            parameters = data.split(";")
+            # TODO: reemplazar por Platform Events para evitar el limite de API
+            # device_type, device_id, value
+            device_type = parameters[0].split('_')[0] #PLANT
+            result = sf.sobjects.DeviceMessage__e.insert({"device_Id__c", parameters[0], "device_type": device_type, "value__c": parameters[1]}) #TODO: Platform events instead?
+        else:
+            # Caso que se recibe desde el ESP-32 para que se devuelva el valor desde SF
+            # TODO: Pasar a funcion especifica de gardenforce
+            if "PLANT" in data:
+                getGardenForceData(data, client, userdata, msg)
 
 def mqtt_publish(client, message):
     client.publish("HPIbCG0C72lcw6g/input", message)
